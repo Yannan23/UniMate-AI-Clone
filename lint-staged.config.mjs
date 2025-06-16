2e4542347781517d7a251d169ecf63f1d023ad7f
Toury <work.yuan@icloud.com>
2025-06-16 09:15:26 +0800

    chore: add commitlint and husky (#36)
    * chore: add commitlint and husky with lint-staged
    
    * chore: add TypeScript as a development dependency

---
+++ lint-staged.config.mjs
@@ -0,0 +1,15 @@
+import path from "path";
+import process from "process";
+
+const buildEslintCommand = (filePaths) =>
+  `next lint --file ${filePaths
+    .map((filePath) => path.relative(process.cwd(), filePath))
+    .join(" --file ")}`;
+
+const config = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{ts,tsx}": () => "npx tsc -p tsconfig.json --noEmit",
+  "*.{js,mjs,jsx,ts,tsx,json,css,md}": ["npx prettier --check"],
+};
+
+export default config;
